2c71cc1a761c9c78768381e6fc04e6357791e4ef
MDON <126775022+StinkHorns@users.noreply.github.com>
2023-10-31 12:24:32 +0100

    Close Modal X ok

--- modal.js
+++ modal.js
@@ -12,8 +12,19 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const closeModalBtn = document.querySelector(".close");
+if (closeModalBtn == null) throw new Error("No close button found");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModalBtn.addEventListener("click", closeModal);
+
+// close modal event
+function closeModal() {
+  console.log("in close modsal");
+  if (modalbg == null) throw new Error("No Modal Background found");
+  modalbg.style.display = "none";
+} 
 
 // launch modal form
 function launchModal() {
